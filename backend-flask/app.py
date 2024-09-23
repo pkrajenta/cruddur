1e2737bc3302bbfc1ec0249cf6b8e84fbef81276
Przemyslaw Krajenta <p.krajenta@gmail.com>
2024-09-23 19:19:22 +0000

    aws/json dir xray initial setup

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -29,6 +29,14 @@ provider.add_span_processor(processor)
 trace.set_tracer_provider(provider)
 tracer = trace.get_tracer(__name__)
 
+# X-RAY
+from aws_xray_sdk.core import xray_recorder
+from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
+
+xray_url = os.getenv("AWS_XRAY_URL")
+xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
+XRayMiddleware(app, xray_recorder)
+
 app = Flask(__name__)
 
 #Honeycomb
