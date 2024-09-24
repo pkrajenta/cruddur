670ac05889b84863d7d707acd837933261a62490
Przemyslaw Krajenta <p.krajenta@gmail.com>
2024-09-24 10:23:40 +0000

    Rollbar commented out

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -16,10 +16,10 @@ from services.show_activity import *
 from services.notifications_activities import *
 
 # Rollbar
-import os
-import rollbar
-import rollbar.contrib.flask
-from flask import got_request_exception
+# import os
+# import rollbar
+# import rollbar.contrib.flask
+# from flask import got_request_exception
 
 # Honeycomb
 from opentelemetry import trace
@@ -61,22 +61,22 @@ app = Flask(__name__)
 
 # XRayMiddleware(app, xray_recorder)
 
-rollbar_access_token = os.getenv('ROLLBAR_ACCESS_TOKEN')
-@app.before_first_request
-def init_rollbar():
-    """init rollbar module"""
-    rollbar.init(
-        # access token
-        rollbar_access_token,
-        # environment name
-        'development',
-        # server root directory, makes tracebacks prettier
-        root=os.path.dirname(os.path.realpath(__file__)),
-        # flask already sets up logging
-        allow_logging_basic_config=False)
+# rollbar_access_token = os.getenv('ROLLBAR_ACCESS_TOKEN')
+# @app.before_first_request
+# def init_rollbar():
+#     """init rollbar module"""
+#     rollbar.init(
+#         # access token
+#         rollbar_access_token,
+#         # environment name
+#         'development',
+#         # server root directory, makes tracebacks prettier
+#         root=os.path.dirname(os.path.realpath(__file__)),
+#         # flask already sets up logging
+#         allow_logging_basic_config=False)
 
-    # send exceptions from `app` to rollbar, using flask's signal system.
-    got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
+#     # send exceptions from `app` to rollbar, using flask's signal system.
+#     got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
 
 #Honeycomb
 FlaskInstrumentor().instrument_app(app)
